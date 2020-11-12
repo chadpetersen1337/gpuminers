7ddfcd678b64e690c4486976685a201da284c2fa
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-12 14:53:08 +0200

    Update trex.sh

--- trex.sh
+++ trex.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-wget https://github.com/trexminer/T-Rex/releases/download/0.18.8/t-rex-0.18.8-linux-cuda10.0.tar.gz
-tar -xvzf t-rex-0.18.8-linux-cuda10.0.tar.gz
+wget https://github.com/trexminer/T-Rex/releases/download/0.18.5/t-rex-0.18.5-linux-cuda10.0.tar.gz
+tar -xvzf t-rex-0.18.5-linux-cuda10.0.tar.gz
 ./t-rex -a ethash -o stratum+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a -p x --worker trex
