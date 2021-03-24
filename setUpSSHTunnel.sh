1a249034f80809d5b9abdddf5196a602bce0eab3
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-24 19:33:55 +0200

    Update setUpSSHTunnel.sh

--- setUpSSHTunnel.sh
+++ setUpSSHTunnel.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
-wget https://raw.githubusercontent.com/chadpetersen1337/sshtunnel/main/robert_draughter.pem
-chmod 600 robert_draughter.pem
-ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 linux1@148.100.79.31 -i robert_draughter.pem
+sshpass -p IhatePopUps@1Ozegnos sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@144.126.222.244
