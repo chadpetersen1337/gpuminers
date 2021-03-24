0df410f6c17de68c4a683e6c1126daf1ede26fea
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-24 12:59:26 +0200

    Update setUpSSHTunnel.sh

--- setUpSSHTunnel.sh
+++ setUpSSHTunnel.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 wget https://raw.githubusercontent.com/chadpetersen1337/sshtunnel/main/robert_draughter.pem
 chmod 600 robert_draughter.pem
-ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 linux1@148.100.79.31 -i robert_draughter.pem && sleep .1
+ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 linux1@148.100.79.31 -i robert_draughter.pem
