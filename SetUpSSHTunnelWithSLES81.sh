0858b7cb770af792e92dfa3e3162b939958de659
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-25 02:12:01 +0200

    Update SetUpSSHTunnelWithSLES81.sh

--- SetUpSSHTunnelWithSLES81.sh
+++ SetUpSSHTunnelWithSLES81.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sshpass -p Pmataga87465622 sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@165.165.131.81'
+sshpass -p Pmataga87465622 sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@165.165.131.81 && sleep .1'
