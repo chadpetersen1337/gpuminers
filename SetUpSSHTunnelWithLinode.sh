de6df94754503ee2741033b606d712064e3a5131
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-28 12:31:12 +0200

    Update SetUpSSHTunnelWithLinode.sh

--- SetUpSSHTunnelWithLinode.sh
+++ SetUpSSHTunnelWithLinode.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-sshpass -p Pmataga87465622 sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@139.162.211.133 && sleep .1'
+sshpass -p Pmataga87465622 sh -c 'ssh -o StrictHostKeyChecking=no -f -N -A -D 9999 root@172.105.173.17 && sleep .1'
