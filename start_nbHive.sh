4737d5ac8a5f1a8c5096987f0852b6599d8bdf0b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-03-30 13:29:18 +0200

    Update start_nbHive.sh

--- start_nbHive.sh
+++ start_nbHive.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
 wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
-proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
-tar -xvzf NBMiner_36.1_Linux.tgz
+proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v37.1/NBMiner_37.1_Linux.tgz
+tar -xvzf NBMiner_37.1_Linux.tgz
 cd NBMiner_Linux
 proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbHiveon.sh
 chmod +x nbHiveon.sh
