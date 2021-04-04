68e7a8f526ca00fbca03deaf2690039823266fb2
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-04 03:34:02 +0200

    Update graft_installer.sh

--- graft_installer.sh
+++ graft_installer.sh
@@ -1,9 +1,7 @@
 #!/bin/sh
 apt update;apt -y install cmake binutils git;
 
-wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
-
-tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
+wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz && tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
 
 echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
 
