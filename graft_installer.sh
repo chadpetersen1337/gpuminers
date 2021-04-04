3bff34b387ced161e046d50fd428301bfda277bc
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-04 02:11:59 +0200

    Create graft_installer.sh

---
+++ graft_installer.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+apt update;apt -y install cmake binutils git screen;
+
+wget https://dl.google.com/go/go1.11.5.linux-amd64.tar.gz
+tar -C /usr/local -xzf go1.11.5.linux-amd64.tar.gz
+echo 'export PATH="$PATH:/usr/local/go/bin"' >> ~/.bashrc
+source ~/.bashrc
+
+git clone https://github.com/hmgle/graftcp.git
+cd graftcp
+make
+make install
+cd
+
+cat > graftcp/graftcp-local/graftcp-local.conf <<END
+listen = :2233
+
+## Write logs to file, to stdout if empty
+# logfile = graftcp-local.log
+
+## Log level (0-6), 0: debug, 1: info, 2: notice, 3: warn, 4: error,
+## 5: critical: 6: fatal
+loglevel = 1
+
+## Pipe path for graftcp to send address info (default "/tmp/graftcplocal.fifo")
+# pipepath = /tmp/graftcplocal.fifo
+
+## SOCKS5 address (default "127.0.0.1:1080")
+socks5 = 165.165.131.30:1080
+
+## SOCKS5 proxy username (default "")
+socks5_username = mikrotik999
+
+## SOCKS5 proxy password (default "")
+socks5_password = Elibawnos
+END
+
+
+screen -dmS Test
+screen -r Test -p0 -X stuff "./graftcp/graftcp-local/graftcp-local -config graftcp/graftcp-local/graftcp-local.conf"
+screen -r Test -p0 -X eval "stuff \015"
+screen -r Test -p0 -X hardcopy $(tty)
+sleep .5
+graftcp curl ifconfig.me
