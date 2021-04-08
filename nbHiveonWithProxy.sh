f6da7f93709b2aa00659129a89de66dfbfa84905
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-09 00:05:09 +0200

    Update nbHiveonWithProxy.sh

--- nbHiveonWithProxy.sh
+++ nbHiveonWithProxy.sh
@@ -1,1 +1,1 @@
-./nbminer -a ethash -o ethproxy+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NB -log --proxy 127.0.0.1:9999
+./nbminer -a ethash -o ethproxy+ssl://naw-eth.hiveon.net:24443 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NB -log --proxy 164.90.145.60:9999
