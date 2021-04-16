6661e21c7f9d18fe3bd982a5d775e995cd80953b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-16 13:56:22 +0200

    Update graft_installerWsocks2.sh

--- graft_installerWsocks2.sh
+++ graft_installerWsocks2.sh
@@ -8,17 +8,23 @@ cd
 
 cat > graftcp/graftcp-local/graftcp-local.conf <<END
 listen = :2233
+
 ## Write logs to file, to stdout if empty
 # logfile = graftcp-local.log
+
 ## Log level (0-6), 0: debug, 1: info, 2: notice, 3: warn, 4: error,
 ## 5: critical: 6: fatal
 loglevel = 1
+
 ## Pipe path for graftcp to send address info (default "/tmp/graftcplocal.fifo")
 # pipepath = /tmp/graftcplocal.fifo
+
 ## SOCKS5 address (default "127.0.0.1:1080")
 socks5 = 45.56.121.195:9999
+
 ## SOCKS5 proxy username (default "")
 #socks5_username = mikrotik999
+
 ## SOCKS5 proxy password (default "")
 #socks5_password = Elibawnos
 END
