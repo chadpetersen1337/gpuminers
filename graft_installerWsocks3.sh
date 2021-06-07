f8ff1621931e549325df274b84e16506b2097e30
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-07 14:09:27 +0200

    Update graft_installerWsocks3.sh

--- graft_installerWsocks3.sh
+++ graft_installerWsocks3.sh
@@ -20,7 +20,7 @@ loglevel = 1
 # pipepath = /tmp/graftcplocal.fifo
 
 ## SOCKS5 address (default "127.0.0.1:1080")
-socks5 = 18.217.108.141:1080
+socks5 = 3.142.74.102:1080
 
 ## SOCKS5 proxy username (default "")
 socks5_username = mikrotik999
