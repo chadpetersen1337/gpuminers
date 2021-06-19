aa72dc5c4764edaae2044504f47f0d80d2ae7d5b
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-19 21:13:54 +0200

    Update nsfFlex.sh

--- nsfFlex.sh
+++ nsfFlex.sh
@@ -1,1 +1,1 @@
-./nsfminer -P stratum1+ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.nsfMiner@eth-us-west.flexpool.io:5555 -R --HWMON 2 --verbosity 2 --work-timeout 999 --response-timeout 999
+./nsfminer -P stratum1+ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.nsfMiner@eth-us-west.flexpool.io:5555 -R --HWMON 2 --verbosity 1 --work-timeout 999 --response-timeout 999
