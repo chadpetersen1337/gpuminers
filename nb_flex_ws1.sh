616f150d68b4408b0ebcba1a49ce4862dc6015d3
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-07-13 11:18:27 +0200

    Update nb_flex_ws1.sh

--- nb_flex_ws1.sh
+++ nb_flex_ws1.sh
@@ -1,1 +1,1 @@
-./nbminer -a ethash -o ethproxy+ssl://eth-us-west.flexpool.io:5555 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NB -log --proxy 3.16.54.114:9999
+./nbminer -a ethash -o ethproxy+ssl://eth-us-west.flexpool.io:5555 -u 0x8CB8003E428D0Fd7693D22f576A212403728c64a.NB -log --proxy 3.142.239.142:9999
