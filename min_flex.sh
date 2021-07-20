8d125cf699e203d67d5828b659effc91b4587a21
chadpetersen1337 <68467197+chadpetersen1337@users.noreply.github.com>
2021-07-20 16:18:38 +0200

    Update min_flex.sh

--- min_flex.sh
+++ min_flex.sh
@@ -1,1 +1,1 @@
-./miniZ --url=ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.miniZ@eth-us-west.flexpool.io:5555 --log --extra --latency --mt-auto --list-modes --oc2
+./miniZ --url=ssl://0x8CB8003E428D0Fd7693D22f576A212403728c64a.miniZ@eth-us-west.flexpool.io:5555 --log --extra --latency --mt-auto --list-modes --ocX --mt-dump
